a69c535dfd4b0b77eb765f1731658a31ba096740
vladv <vlad.vakalyk@gmail.com>
2020-04-08 23:42:32 +0300

    UserObserver

---
+++ app/Observers/UserObserver.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Observers;
+
+use App\User;
+use Illuminate\Http\Request;
+
+
+class UserObserver
+{
+    /**
+     * Handle the user "created" event.
+     *
+     * @param  \App\User  $user
+     * @return void
+     */
+    public function created(User $user)
+    {
+        $user->assignRole(['name' => User::getRoleName(User::$role)]);
+    }
+
+    public function creating(User $user)
+    {
+
+    }
+
+    /**
+     * Handle the user "updated" event.
+     *
+     * @param  \App\User  $user
+     * @return void
+     */
+    public function updated(User $user)
+    {
+        //
+    }
+
+    /**
+     * Handle the user "deleted" event.
+     *
+     * @param  \App\User  $user
+     * @return void
+     */
+    public function deleted(User $user)
+    {
+        //
+    }
+
+    /**
+     * Handle the user "restored" event.
+     *
+     * @param  \App\User  $user
+     * @return void
+     */
+    public function restored(User $user)
+    {
+        //
+    }
+
+    /**
+     * Handle the user "force deleted" event.
+     *
+     * @param  \App\User  $user
+     * @return void
+     */
+    public function forceDeleted(User $user)
+    {
+        //
+    }
+}
